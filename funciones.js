ecef301da711d4871f4fda6d4429700f3938f9a0
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-25 13:24:03 +0200

    demostración funciones

---
+++ funciones.js
@@ -0,0 +1,20 @@
+const PI = 3.14;
+function saludar() {
+  console.log("Hola mundo");
+}
+function saludarPorNombre(nombre, saludo = "Hola") {
+  // console.log("Hola " + nombre);
+  console.log(`${saludo} ${nombre}`);
+}
+function calcularCircunferencia(r = 1) {
+  const c = 2 * PI * r;
+  return c;
+}
+
+saludar();
+saludarPorNombre("Alberto");
+saludarPorNombre("Alberto", "Hello");
+
+let radio = 5;
+let circunferencia = calcularCircunferencia(radio);
+console.log(circunferencia);
---
+++ iva.js
@@ -0,0 +1,6 @@
+const ivaNormal = 10;
+const precio = 50;
+
+const importe = precio + (precio * ivaNormal) / 100;
+importe = 9;
+console.log("El precio sin IVA es: " + precio);
